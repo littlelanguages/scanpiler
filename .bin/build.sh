cac6d0de9fe6ca51c8151ea52aa062f4eebef4fc
Graeme Lockley <graemel@no9.co.za>
2020-09-19 09:03:37 +0200

    chore: generate the scanner using deno-cli

--- .bin/build.sh
+++ .bin/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# deno run --allow-read --allow-write "https://raw.githubusercontent.com/littlelanguages/scanpiler-cli/main/mod.ts" deno --verbose parser/scanner.ll
-
+deno run --allow-read --allow-write "https://raw.githubusercontent.com/littlelanguages/scanpiler-cli/main/mod.ts" deno --verbose parser/scanner.ll
+deno fmt parser/scanner.ts
