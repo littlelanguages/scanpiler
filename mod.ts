e0ff3961dba1477b9cd7f5370b356813243a26af
Graeme Lockley <graemel@no9.co.za>
2020-11-06 05:22:38 +0000

    chore: add dfa and nfa into the mod export

--- mod.ts
+++ mod.ts
@@ -3,3 +3,5 @@ export * from "./la/definition.ts";
 export * from "./parser/errors.ts";
 export * from "./la/la.ts";
 export * from "./la/fa.ts";
+export * from "./la/dfa.ts";
+export * from "./la/nfa.ts";
