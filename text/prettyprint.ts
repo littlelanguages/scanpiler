f8de6d45561785d8a1a81039779539800a181693
Graeme Lockley <graemel@no9.co.za>
2020-11-06 04:59:21 +0000

    chore: upgrade pretty print

--- text/prettyprint.ts
+++ text/prettyprint.ts
@@ -1,1 +1,1 @@
-export * from "https://raw.githubusercontent.com/littlelanguages/deno-lib-text-prettyprint/0.3.1/mod.ts";
+export * from "https://raw.githubusercontent.com/littlelanguages/deno-lib-text-prettyprint/0.3.2/mod.ts";
